165860ec47deb3a3e4fd8209441da9946c45bfae
samandargofurov <samandargofurov236@gmail.com>
2024-06-22 15:04:58 +0500

    done

---
+++ index.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('scroll', function () {
+        const header = document.querySelector('header');
+        header.classList.toggle('sticky', window.scrollY > 0)
+    })
+
+    const menuBtn = document.querySelector('.menu-btn')
+    const navigation = document.querySelector('.navigation')
+
+    menuBtn.addEventListener('click', () => {
+        menuBtn.classList.toggle('active')
+        navigation.classList.toggle('active')
+    })
+})
